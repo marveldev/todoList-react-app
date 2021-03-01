a4052fc85ff18d128d6a33eb8438044b4ca9aa0f
Marvel Dev <marvellousalika48@gmail.com>
2021-03-01 21:23:05 +0100

    add Form props
    - add TodoList props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { React, useState } from 'react'
-import { Form } from './component'
+import { Form,TodoList } from './component'
 import './App.css'
 
 function App() {
   const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState([])
   return (
     <div>
       <h1>My TodoList App</h1>
-      <Form setInputText={setInputText} />
+      <Form
+        setInputText={setInputText}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+      />
     </div>
   );
 }
