8cd8925905cc6c598b3e89acd9718cd7d9bf44c0
Marvel Dev <marvellousalika48@gmail.com>
2021-03-14 18:48:32 +0100

    modify file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { React, useState } from 'react'
-import { Form,TodoList } from './component'
-import './App.css'
+import { useState } from 'react'
+import { Form, TodoList } from './component'
+import './index.css'
 
-function App() {
-  const [inputText, setInputText] = useState('')
-  const [todos, setTodos] = useState([])
+const App = () => {
+  const [todoList, setTodoList] = useState([])
   return (
     <div>
       <h1>My TodoList App</h1>
       <Form
-        setInputText={setInputText}
-        inputText={inputText}
-        todos={todos}
-        setTodos={setTodos}
+        todoList={todoList}
+        setTodoList={setTodoList}
       />
       <TodoList
-        todos={todos}
-        setTodos={setTodos}
+        todoList={todoList}
+        setTodoList={setTodoList}
       />
     </div>
-  );
+  )
 }
 
 export default App
