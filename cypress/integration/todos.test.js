31c474f9be25390b2222354db2fb1aff956c079d
marveldev <marvellousalika48@gmail.com>
2021-11-05 15:02:35 +0100

    testing with cypress

---
+++ cypress/integration/todos.test.js
@@ -0,0 +1,50 @@
+describe('Todos', () => {
+  beforeEach(() => {
+    cy.visit('/') // before each test in this block visit this url.
+  })
+
+  const todoText = 'Dancing'
+
+  //user should be able to add task
+  it('user should be able to add a todo and clear input', () => {
+    cy.findByRole('textbox').type(todoText)
+    cy.findByRole('button', {  name: /submit/i}).click()
+    cy.contains(todoText).should('exist')
+    cy.findByRole('textbox').should('contain', '')
+  })
+
+  //user should be able to filter between todos
+  it('user should be able to filter between todos', () => {
+    cy.findByRole('combobox').select('Completed')
+
+  })
+
+  //user should be able to toggle the state of a task
+  it('user should be able to toggle the state of a todo', () => {
+    cy.get('.todo-text').first().then(($todo) => {
+      if ($todo.hasClass('completed')) {
+        cy.get('.todo').find('.complete-button').first().click()
+        cy.get('.todo-text').first().should('not.have.class', 'completed')
+      } else {
+        cy.get('.todo').find('.complete-button').first().click()
+        cy.get('.todo-text').first().should('have.class', 'completed')
+      }
+    })
+  })
+
+  //user should be able to edit a task
+  it('user should be able to edit a todo', () => {
+    cy.get('.todo').find('.edit-button').first().click()
+    cy.contains('Enter new Todo').should('be.visible')
+    cy.get('.new-todo-input').clear().type('Singing')
+    cy.get('.edit-modal').find('.form').submit()
+    cy.get('.todo-text').first().should('contain', 'Singing')
+  })
+
+  //user should be able to delete a task
+  it('user should be able to delete a todo', () => {
+    cy.get('.todo').find('.trash-button').first().click()
+    cy.findByText(/are you sure, you want to delete\?/i).should('be.visible')
+    cy.findByRole('button', {  name: /confirm/i}).click()
+  })
+})
