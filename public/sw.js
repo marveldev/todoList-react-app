dbaa1549ad47b14b70e47c7f6ec8529ddd0b2b89
marveldev <marvellousalika48@gmail.com>
2022-01-07 17:11:33 +0100

    add cache data to service worker

--- public/sw.js
+++ public/sw.js
@@ -6,20 +6,27 @@ this.addEventListener("install", (event) => {
         '/static/js/main.chunk.js',
         '/static/js/vendors~main.chunk.js',
         '/static/js/bundle.js',
-        '/bootstrap.min.css',
-        '/icon-192x192.png',
+        '/static/media/checklist-illustration.95d386095a8daaf7c472fb9a542bda58.svg',
+        '/static/media/complete-sound.02c736a3d1f0d82cb3d6.mp3',
+        '/static/media/edit-icon.8e3527b31305cf8206abeb01afef2964.svg',
+        '/static/media/delete-icon.2eb4d49ac5d02fcffda2f1995041433c.svg',
         '/index.html',
-        '/'
+        '/manifest.json',
+        '/',
+        '/icon-192x192.png',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
       ])
     })
   )
 })
 
 this.addEventListener("fetch", (event) => {
-  event.respondWith((async () => {
-    const cachedResponse = await caches.match(event.request)
-    if (cachedResponse) {
-      return cachedResponse
-    }
-  })())
+  if (!navigator.onLine) {
+    event.respondWith((async () => {
+      const cachedResponse = await caches.match(event.request)
+      if (cachedResponse) {
+        return cachedResponse
+      }
+    })())
+  }
 })
